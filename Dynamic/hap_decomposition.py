4a75cae8dac85d416982e192f5aa796bc3600c34
youngkuang <lyg1992@gmail.com>
2022-09-03 17:11:52 +0900

    https://github.com/glory0224/Baekjoon.git

--- Dynamic/hap_decomposition.py
+++ Dynamic/hap_decomposition.py
@@ -26,5 +26,5 @@ for i in range(2, 201):
     for j in range(2, 201):
         # dp[2][2] = dp[2][1] + dp[1][1] -> 3 = 2 + 1 
         dp[i][j] = (dp[i][j-1]+dp[i-1][j])% 1000000000
-
+ 
 print(dp[k][n])
