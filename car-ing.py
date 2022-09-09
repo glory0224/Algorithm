837ec10f15e5af64578e8c7df10dd553f39ee2d2
youngkuang <lyg1992@gmail.com>
2022-09-09 20:56:13 +0900

    algorithm

---
+++ car-ing.py
@@ -0,0 +1,13 @@
+# 6064번 백준 카잉 달력 문제 
+
+def num(m, n, x, y):
+    while x <= m * n:
+        if (x - y) % n == 0:
+            return x
+        x += m
+    return -1
+
+t = int(input())
+for i in range(t):
+    m, n, x, y = map(int, input().split())
+    print(num(m, n, x, y))
