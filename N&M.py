a30d8bbe9b4e4c104d26d10253fba3a9f3f26cfb
youngkuang <lyg1992@gmail.com>
2022-09-11 21:56:59 +0900

    recursive algorithm

---
+++ N&M.py
@@ -0,0 +1,21 @@
+# 백준 15649번 N과 M문제 
+# 재귀함수를 잘 이용한 문제 
+
+
+n,m = list(map(int,input().split()))
+ 
+s = []
+ 
+def dfs():
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(1,n+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop() # 재귀함수가 종료될 때마다 pop 실행, 중복된 수를 빼내기 위한 코드 
+ 
+dfs()
+ 
