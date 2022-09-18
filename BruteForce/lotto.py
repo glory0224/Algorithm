26c5a1b46cbb55bbb9f40b59fd4fb4833cd39375
youngkuanglee <77872624+glory0224@users.noreply.github.com>
2022-09-18 22:30:36 +0900

    Update and rename lotto.py to BruteForce/lotto.py

--- BruteForce/lotto.py
+++ BruteForce/lotto.py
@@ -21,26 +21,3 @@ while True:
         print() # print end 옵션으로 옆으로 쭉 나열되기 때문에 print()로 줄바꿈 처리 해준다. 
     print() # 출력 답에 개행이 되어 있기 때문에 print를 한번 더 해줘서 서로 간격을 띄어준다. 
 
-
-# dfs로 문제 풀기 
-
-def dfs(start, depth):
-    if depth == 6:
-        for i in range(6):
-            print(combi[i], end=' ')        
-        print()
-        return    
-    for i in range(start, len(s)):
-        combi[depth] = s[i]
-        dfs(i + 1, depth + 1)
-        
-combi = [0 for i in range(13)] # 6개의 인덱스로 조합을 짤 때 최대 수가 0 ~ 13 사이의 수이기 때문에 이렇게 지정
-while True:
-    s = list(map(int, input().split()))
-    if s[0] == 0:
-        break
-    del s[0]
-    dfs(0, 0)
-    print()
-
-
