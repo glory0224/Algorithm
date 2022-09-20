ddfc456ed3d256119568b0f74ae5ad8672ab3c14
youngkuang <lyg1992@gmail.com>
2022-09-20 21:21:45 +0900

    algorithm

---
+++ leave_company.py
@@ -0,0 +1,25 @@
+# 백준 14501번 퇴사 
+
+# https://pacific-ocean.tistory.com/199
+
+N = int(input())
+T = []
+P = []
+dp = []
+
+for i in range(N):
+    t , p = map(int, input().split())
+    T.append(t)
+    P.append(p)
+    dp.append(p)
+dp.append(0)
+
+for i in range(N-1, -1, -1):
+    # 일수를 넘어가면 어차피 퇴사이기 때문에 값만 넘겨준다. 
+    if T[i] + i > N:
+        dp[i] = dp[i +1]
+    else:
+        dp[i] = max(dp[i+1], P[i] + dp[i+T[i]])
+        
+print(dp[0])
+
